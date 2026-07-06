0384d69b2d7343c4a7f19d18110e4b8a3876f7b7
arish-704 <arishshahid0704@gmail.com>
2026-07-06 20:23:46 +0530

    Initial Spring boot project setup

---
+++ src/main/java/com/arish/shoppersclub/ShoppersclubApplication.java
@@ -0,0 +1,13 @@
+package com.arish.shoppersclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppersclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppersclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arish/shoppersclub/ShoppersclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arish.shoppersclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppersclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
